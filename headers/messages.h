b9621945d8291d2a726a6b7511d1374ef67fe953
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2021-10-27 00:34:29 +0330

    Removed messages regarding file handling and defined unknown error message

--- headers/messages.h
+++ headers/messages.h
@@ -5,10 +5,6 @@ namespace Messages
 {
     void noItemSelected(QWidget *parent = nullptr);
     void noSheetFound(QWidget *parent = nullptr);
-    void saveFailed(QWidget *parent = nullptr);
-    void openFailed(QWidget *parent = nullptr);
-    void unsupportedFormat(QWidget *parent = nullptr);
-    void corruptFile(QWidget *parent = nullptr);
     bool confirmToDelete(int size, QWidget *parent = nullptr);
     void unknownError(QWidget *parent = nullptr);
     void cannotShowItem(QWidget *parent = nullptr);
--- sources/messages.cpp
+++ sources/messages.cpp
@@ -10,42 +10,6 @@ void Messages::noSheetFound(QWidget *parent)
     noSheet.setInformativeText("Please open a file or click <b>New sheet<\b>");
     noSheet.exec();
 }
-void Messages::saveFailed(QWidget *parent)
-{
-    QMessageBox saveFailed(parent);
-    saveFailed.setWindowTitle("Failed to save information");
-    saveFailed.setIcon(QMessageBox::Warning);
-    saveFailed.setText("An error occurred when saving information.");
-    saveFailed.setInformativeText("Make sure the selected file is allowed to be saved to.");
-    saveFailed.exec();
-}
-void Messages::openFailed(QWidget *parent)
-{
-    QMessageBox openFailed(parent);
-    openFailed.setIcon(QMessageBox::Warning);
-    openFailed.setWindowTitle("Failed to open file");
-    openFailed.setText("The chosen filename is not available.");
-    openFailed.setInformativeText("Check if the selected file is not moved, renamed or unavailable and then try again.");
-    openFailed.exec();
-}
-void Messages::unsupportedFormat(QWidget *parent)
-{
-    QMessageBox wrongFormat(parent);
-    wrongFormat.setIcon(QMessageBox::Critical);
-    wrongFormat.setWindowTitle("Failed to open file");
-    wrongFormat.setText("The selected file is unsupported.");
-    wrongFormat.setInformativeText("Please select another one and try again.");
-    wrongFormat.exec();
-}
-void Messages::corruptFile(QWidget *parent)
-{
-    QMessageBox message(parent);
-    message.setIcon(QMessageBox::Critical);
-    message.setWindowTitle("The file is corrupt");
-    message.setText("The selected file is corrupt or damaged.");
-    message.setInformativeText("Please select another one and try again.");
-    message.exec();
-}
 void Messages::noItemSelected(QWidget *parent)
 {
     QMessageBox message(parent);
@@ -72,5 +36,13 @@ void Messages::cannotShowItem(QWidget *parent)
     message.setIcon(QMessageBox::Information);
     message.setText("The selected information is completely private.");
     message.setInformativeText("Please select another item or make some information public.");
+    message.exec();
+}
+void Messages::unknownError(QWidget *parent)
+{
+    QMessageBox message(parent);
+    message.setWindowTitle("Unknown error");
+    message.setIcon(QMessageBox::Critical);
+    message.setText("An unexpected error has happened.");
     message.exec();
 }
