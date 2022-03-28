a3b1df8b9266a86356304f7f24f585e572126745
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2022-03-28 14:57:35 +0430

    Modification to the info editor
    1- The editor can now access to the fields of information directly to boost the operation.
    2- Removed codes created using macros.
    3- Lambda-expressions leading to reset information is now replaced with specialized functions for reusability.
    4- Improved the way information is passed to the editor.

--- include/infoeditor.h
+++ include/infoeditor.h
@@ -5,23 +5,36 @@
 #include "infocontainer.h"
 
 namespace Ui { class InfoEditor; }
+class PhotoView;
 
 class InfoEditor : public QDialog
 {
     Q_OBJECT
-    InfoContainer &container;
-    Ui::InfoEditor *ui{};
-    class PhotoViewer *photoViewer{};
-private:
-    void setupUi();
-    void initEditor();
-    void initGenderFromContainer();
     Q_DISABLE_COPY_MOVE(InfoEditor)
+    Ui::InfoEditor *ui{};
+    PhotoView *containerPhoto{};
+    void setupEditor();
+private:
+    InfoContainer resetContainer;
+    InfoContainer resultContainer;
 protected:
     void accept() override;
 public:
-    explicit InfoEditor(InfoContainer &information, QWidget *parent = nullptr);
+    explicit InfoEditor(QWidget *parent = nullptr);
+    explicit InfoEditor(const InfoContainer &initContainer, QWidget *parent = nullptr);
     ~InfoEditor();
+    const InfoContainer& infoContainer() const;
+    void setInfoContainer(const InfoContainer &tempContainer);
+public slots:
+    void resetFirstName();
+    void resetLastName();
+    void resetFatherName();
+    void resetBirthday();
+    void resetGender();
+    void resetNationality();
+    void resetBornProvince();
+    void resetPhoto();
+    void resetEditor();
 };
 
 #endif // INFOEDITOR_H
--- src/infoeditor.cpp
+++ src/infoeditor.cpp
@@ -1,13 +1,36 @@
-#include "include/photoviewer.h"
 #include "include/infoeditor.h"
+#include "include/photoview.h"
 #include "ui_infoeditor.h"
 #include <QFileDialog>
-#define INIT_PROPERTY_FROM_CURRENT(PROPERTY) \
-    ui->container##PROPERTY->setText(container.get##PROPERTY());
 
-void InfoEditor::initGenderFromContainer()
+InfoEditor::InfoEditor(QWidget *parent) : QDialog(parent)
 {
-    switch(container.getGender())
+    setupEditor();
+}
+InfoEditor::InfoEditor(const InfoContainer &initContainer, QWidget *parent) : QDialog(parent), resetContainer(initContainer)
+{
+    setupEditor();
+    resetEditor();
+}
+void InfoEditor::resetFirstName()
+{
+    ui->containerFirstName->setText(resetContainer.firstName);
+}
+void InfoEditor::resetLastName()
+{
+    ui->containerLastName->setText(resetContainer.lastName);
+}
+void InfoEditor::resetFatherName()
+{
+    ui->containerFatherName->setText(resetContainer.fatherName);
+}
+void InfoEditor::resetBirthday()
+{
+    ui->containerBirthday->setDate(resetContainer.birthday);
+}
+void InfoEditor::resetGender()
+{
+    switch(resetContainer.gender)
     {
     case InfoContainer::GenderContainer::Male: {
        ui->radioButtonMale->setChecked(true);
@@ -19,90 +42,72 @@ void InfoEditor::initGenderFromContainer()
     }
     }
 }
-void InfoEditor::setupUi()
+void InfoEditor::resetNationality()
+{
+    ui->containerNationality->setText(resetContainer.nationality);
+}
+void InfoEditor::resetBornProvince()
+{
+    ui->containerBornProvince->setText(resetContainer.bornProvince);
+}
+void InfoEditor::resetPhoto()
+{
+    containerPhoto->setCurrentPhoto(resetContainer.photo);
+}
+void InfoEditor::resetEditor()
+{
+    resetFirstName();
+    resetLastName();
+    resetFatherName();
+    resetBirthday();
+    resetGender();
+    resetNationality();
+    resetBornProvince();
+    resetPhoto();
+}
+void InfoEditor::setupEditor()
 {
     ui = new Ui::InfoEditor;
     ui->setupUi(this);
 
+    containerPhoto = new PhotoView(this);
+    containerPhoto->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+    ui->layoutPhoto->insertWidget(0, containerPhoto);
+
     ui->containerBirthday->setDate(QDate::currentDate());
-
-    photoViewer = new PhotoViewer;
-    photoViewer->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
-    ui->layoutPhoto->insertWidget(0, photoViewer);
-
-    connect(ui->buttonResetFirstName, &QPushButton::clicked, this, [=]()
-    {
-        INIT_PROPERTY_FROM_CURRENT(FirstName)
-    });
-    connect(ui->buttonResetLastName, &QPushButton::clicked, this, [=]()
-    {
-        INIT_PROPERTY_FROM_CURRENT(LastName)
-    });
-    connect(ui->buttonResetFatherName, &QPushButton::clicked, this, [=]()
-    {
-        INIT_PROPERTY_FROM_CURRENT(FatherName)
-    });
-    connect(ui->buttonResetNationality, &QPushButton::clicked, this, [=]()
-    {
-        INIT_PROPERTY_FROM_CURRENT(Nationality)
-    });
-    connect(ui->buttonResetBornProvince, &QPushButton::clicked, this, [=]()
-    {
-        INIT_PROPERTY_FROM_CURRENT(BornProvince)
-    });
-    connect(ui->buttonResetGender, &QPushButton::clicked, this, &InfoEditor::initGenderFromContainer);
-    connect(ui->buttonResetBirthday, &QPushButton::clicked, this, [=]()
-    {
-        ui->containerBirthday->setDate(container.getBirthday());
-    });
-    connect(ui->buttonResetPhoto, &QPushButton::clicked, this, [=]()
-    {
-        photoViewer->setCurrentPhoto(container.getPhoto());
-    });
+    connect(ui->buttonResetFirstName, &QPushButton::clicked, this, &InfoEditor::resetFirstName);
+    connect(ui->buttonResetLastName, &QPushButton::clicked, this, &InfoEditor::resetLastName);
+    connect(ui->buttonResetFatherName, &QPushButton::clicked, this, &InfoEditor::resetFatherName);
+    connect(ui->buttonResetGender, &QPushButton::clicked, this, &InfoEditor::resetGender);
+    connect(ui->buttonResetBirthday, &QPushButton::clicked, this, &InfoEditor::resetBirthday);
+    connect(ui->buttonResetNationality, &QPushButton::clicked, this, &InfoEditor::resetNationality);
+    connect(ui->buttonResetBornProvince, &QPushButton::clicked, this, &InfoEditor::resetBornProvince);
+    connect(ui->buttonResetPhoto, &QPushButton::clicked, this, &InfoEditor::resetPhoto);
+    connect(ui->buttonResetAll, &QPushButton::clicked, this, &InfoEditor::resetEditor);
     connect(ui->buttonLocalBrowser, &QPushButton::clicked, this, [=]()
     {
-        QFileDialog dialog;
-        dialog.setAcceptMode(QFileDialog::AcceptOpen);
-        dialog.setNameFilters({"JPG files (*.jpg *.jpeg)", "PNG files (*.png)", "BMP files (*.bmp)"});
-        dialog.setFileMode(QFileDialog::ExistingFile);
-        if(dialog.exec()) {
-            photoViewer->setCurrentPhoto(dialog.selectedFiles().constFirst());
+        QFileDialog fileBrowser(this, "Select a photo to continue", QDir::homePath());
+        fileBrowser.setAcceptMode(QFileDialog::AcceptOpen);
+        fileBrowser.setNameFilters({"JPG files (*.jpg *.jpeg)", "PNG files (*.png)", "BMP files (*.bmp)"});
+        fileBrowser.setFileMode(QFileDialog::ExistingFile);
+        if(fileBrowser.exec()) {
+            containerPhoto->setCurrentPhoto(fileBrowser.selectedFiles().constFirst());
         }
     });
-#define DEFINE_RESET_ALL_CONNECTION(PROPERTY) \
-    connect(ui->buttonResetAll, &QPushButton::clicked, ui->buttonReset##PROPERTY, &QPushButton::clicked);
-
-    DEFINE_RESET_ALL_CONNECTION(FirstName)
-    DEFINE_RESET_ALL_CONNECTION(LastName)
-    DEFINE_RESET_ALL_CONNECTION(FatherName)
-    DEFINE_RESET_ALL_CONNECTION(Nationality)
-    DEFINE_RESET_ALL_CONNECTION(BornProvince)
-    DEFINE_RESET_ALL_CONNECTION(Birthday)
-    DEFINE_RESET_ALL_CONNECTION(Gender)
-    DEFINE_RESET_ALL_CONNECTION(Photo)
-
-#undef DEFINE_RESET_ALL_CONNECTION
-}
-InfoEditor::InfoEditor(InfoContainer &information, QWidget *parent) : QDialog(parent), container(information)
-{
-    setupUi();
-    initEditor();
 }
 InfoEditor::~InfoEditor()
 {
-    delete photoViewer;
     delete ui;
 }
-void InfoEditor::initEditor()
+const InfoContainer &InfoEditor::infoContainer() const
 {
-    INIT_PROPERTY_FROM_CURRENT(FirstName)
-    INIT_PROPERTY_FROM_CURRENT(LastName)
-    INIT_PROPERTY_FROM_CURRENT(FatherName)
-    INIT_PROPERTY_FROM_CURRENT(Nationality)
-    INIT_PROPERTY_FROM_CURRENT(BornProvince)
-    initGenderFromContainer();
-    ui->containerBirthday->setDate(container.getBirthday());
-    photoViewer->setCurrentPhoto(container.getPhoto());
+    return resultContainer;
+}
+void InfoEditor::setInfoContainer(const InfoContainer &tempContainer)
+{
+    resetContainer = tempContainer;
+    resultContainer = InfoContainer();
+    resetEditor();
 }
 void InfoEditor::accept()
 { 
@@ -117,23 +122,19 @@ void InfoEditor::accept()
             return;
         }
     }
-#define APPLY_TEXTUAL_PROPERTY(PROPERTY) \
-    container.set##PROPERTY(ui->container##PROPERTY->text());
-
-    APPLY_TEXTUAL_PROPERTY(FirstName)
-    APPLY_TEXTUAL_PROPERTY(LastName)
-    APPLY_TEXTUAL_PROPERTY(FatherName)
-    APPLY_TEXTUAL_PROPERTY(BornProvince)
-    APPLY_TEXTUAL_PROPERTY(Nationality)
-
-#undef APPLY_TEXTUAL_PROPERTY
+    resultContainer.firstName = ui->containerFirstName->text();
+    resultContainer.lastName = ui->containerLastName->text();
+    resultContainer.fatherName = ui->containerFatherName->text();
     if(ui->radioButtonMale->isChecked()) {
-        container.setGender(InfoContainer::GenderContainer::Male);
+        resultContainer.gender = InfoContainer::GenderContainer::Male;
     }
     else if(ui->radioButtonFemale->isChecked()) {
-        container.setGender(InfoContainer::GenderContainer::Female);
+        resultContainer.gender = InfoContainer::GenderContainer::Female;
     }
-    container.setBirthday(ui->containerBirthday->date());
-    container.setPhoto(photoViewer->getCurrentPhoto());
+    resultContainer.birthday = ui->containerBirthday->date();
+    resultContainer.bornProvince = ui->containerBornProvince->text();
+    resultContainer.nationality = ui->containerNationality->text();
+    resultContainer.photo = containerPhoto->currentPhoto();
+    resultContainer.lastModification = QDateTime::currentDateTime();
     QDialog::accept();
 }
