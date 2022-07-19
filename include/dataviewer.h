4c045a1b1439c990dd0aacd0395a8b57d5cf7e45
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2022-07-19 17:52:44 +0430

    Modification to the data viewer
    Added essential functionality to the component.

--- include/dataviewer.h
+++ include/dataviewer.h
@@ -2,6 +2,7 @@
 #define DATAVIEWER_H
 
 #include <QDialog>
+#include "include/datacontainer.h"
 
 namespace Ui { class DataViewer; }
 class NamedPhotoViewer;
@@ -10,12 +11,28 @@ class DataViewer : public QDialog
 {
     Q_OBJECT
     Ui::DataViewer *ui{};
+    DataContainer m_container{};
     NamedPhotoViewer *w_photo{};
 private:
     void setupViewer();
+    void fillBackground(QPainter *painter);
+protected:
+    virtual void paintEvent(QPaintEvent *event);
 public:
     explicit DataViewer(QWidget *parent = nullptr);
+    explicit DataViewer(const DataContainer &target, QWidget *parent = nullptr);
     ~DataViewer();
+
+    const DataContainer& container() const;
+    void setContainer(const DataContainer &container);
+public slots:
+    void resetFullName();
+    void resetFatherName();
+    void resetBirthday();
+    void resetGender();
+    void resetCountry();
+    void resetPhoto();
+    void resetViewer();
 };
 
 #endif // DATAVIEWER_H
--- src/dataviewer.cpp
+++ src/dataviewer.cpp
@@ -21,7 +21,96 @@ DataViewer::DataViewer(QWidget *parent) : QDialog(parent)
 {
     setupViewer();
 }
+DataViewer::DataViewer(const DataContainer &target, QWidget *parent) : QDialog(parent)
+{
+    setupViewer();
+    setContainer(target);
+}
 DataViewer::~DataViewer()
 {
     delete ui;
+}
+const DataContainer &DataViewer::container() const
+{
+    return m_container;
+}
+void DataViewer::setContainer(const DataContainer &container)
+{
+    m_container = container;
+    resetViewer();
+}
+
+void DataViewer::resetFullName()
+{
+    w_photo->setPrimaryText(m_container.fullName());
+}
+void DataViewer::resetFatherName()
+{
+    ui->containerFatherName->setText(m_container.fatherName());
+}
+void DataViewer::resetBirthday()
+{
+    ui->containerBirthday->setText(m_container.birthday().toString());
+}
+void DataViewer::resetGender()
+{
+    using enum DataContainer::GenderContainer;
+    switch(m_container.gender()) {
+    case Male: {
+        ui->containerGender->setText("Male");
+        break;
+    }
+    case Female: {
+        ui->containerGender->setText("Female");
+        break;
+    }
+    }
+}
+void DataViewer::resetCountry()
+{
+    ui->containerCountry->setText(m_container.country());
+}
+void DataViewer::resetPhoto()
+{
+    w_photo->setPhoto(m_container.photo());
+}
+void DataViewer::resetViewer()
+{
+    resetFullName();
+    resetFatherName();
+    resetBirthday();
+    resetGender();
+    resetCountry();
+    resetPhoto();
+    w_photo->setSecondaryText("Date modified on " + m_container.modified().toString());
+    ui->labelDateCreated->setText("Date created: " + m_container.created().toString());
+}
+
+#include <QPainter>
+void DataViewer::fillBackground(QPainter *painter)
+{
+    const qreal start = 0.1;
+
+    QLinearGradient gradient(QPointF(0, 0), QPointF(1, 1));
+    gradient.setCoordinateMode(QGradient::ObjectMode);
+
+    using enum DataContainer::GenderContainer;
+    switch(m_container.gender()) {
+    case Male: {
+        gradient.setColorAt(start, QColorConstants::Svg::lightblue);
+        break;
+    }
+    case Female: {
+        gradient.setColorAt(start, QColorConstants::Svg::lightpink);
+        break;
+    }
+    }
+    gradient.setColorAt(0.4, Qt::white);
+    painter->fillRect(rect(), gradient);
+}
+void DataViewer::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    fillBackground(&painter);
+    QWidget::paintEvent(event);
 }
