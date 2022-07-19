3cf9435a4bbefe5e641882e02c4661c7a9030df8
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2022-07-19 17:11:56 +0430

    Test on how the data viewer works.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
+#include "include/dataviewer.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
-
+    DataViewer view;
+    view.exec();
 
     return app.exec();
 }
