e8f565cc914e95e580985fc786241bf1636df0bb
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2022-07-21 14:15:40 +0430

    Test how the data editor works.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,23 @@ int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
+    DataContainer container;
+    container.setFirstName("John");
+    container.setLastName("Speraw");
+    container.setFatherName("Michael");
+    container.setBirthday(QDate(1971, 6, 9));
+    container.setCountry("Japan");
+    container.setGender(DataContainer::GenderContainer::Female);
+    container.setPhoto(QPixmap("C:/Users/Amir Mohsen Ghasemi/Downloads/John Speraw.jpg"));
+
     DataEditor editor;
+    editor.setContainer(&container);
+
     if(editor.exec() == QDialog::Accepted) {
-        DataViewer view(editor.container());
+        if(editor.container() != &container) {
+            qDebug() << "FAILED! Checkout and try again!";
+        }
+        DataViewer view(*editor.container());
         view.exec();
     }
 
